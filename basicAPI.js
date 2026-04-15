b965b504414964fa67e5631413b5b50cb3d6ff7b
hiraisrar123 <hiraisrar47@gmail.com>
2026-04-15 18:07:11 +0500

    hi

--- basicAPI.js
+++ basicAPI.js
@@ -2,6 +2,13 @@ const http = require("http");
 
 http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ name: "Hello, Hira!", dep: "CS"}));
-    res.end();
-}).listen(1000);
+
+    const data = {
+        name: "Hello, Hira!",
+        dep: "CS"
+    };
+
+    res.end(JSON.stringify(data));
+}).listen(1000, () => {
+    console.log("Server running on http://localhost:1000");
+});
