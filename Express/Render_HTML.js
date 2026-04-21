fb2dbefd6aa4b140dab7cc574a688dd1e398aac8
hiraisrar123 <hiraisrar47@gmail.com>
2026-04-21 19:36:48 +0500

    hira

--- Express/Render_HTML.js
+++ Express/Render_HTML.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("", (req, res) => {
-  res.send(
-    `<input type="text" placeholder="Your Name"/>
-      <a href="/contact">Go to Contact Us Page</a>`
-  )
-});
+// app.get("", (req, res) => {
+//   res.send(
+//     `<input type="text" placeholder="Your Name"/>
+//       <a href="/contact">Go to Contact Us Page</a>`
+//   )
+// });
 
-app.get("/contact", (req, res) => {
-  res.send(
-    `<h1>Contact Us</h1>
-     <p>Feel free to reach out to us!</p>
-     <a href="/about">Go to About Page</a>`
-  )
-});
+// app.get("/contact", (req, res) => {
+//   res.send(
+//     `<h1>Contact Us</h1>
+//      <p>Feel free to reach out to us!</p>
+//      <a href="/about">Go to About Page</a>`
+//   )
+// });
 
-app.get("/about", (req, res) => {
-  res.send([
-    { name: "Hira", email: "hira@example.com" },
-    { name: "Bilal", email: "bilal@example.com" },
-    { name: "John Doe", email: "john@example.com" }
-  ])
-});
+// app.get("/about", (req, res) => {
+//   res.send([
+//     { name: "Hira", email: "hira@example.com" },
+//     { name: "Bilal", email: "bilal@example.com" },
+//     { name: "John Doe", email: "john@example.com" }
+//   ])
+// });
 
-app.listen(2000, () => {
-  console.log("Server is running on port 2000");
-});
+// app.listen(2000, () => {
+//   console.log("Server is running on port 2000");
+// });
---
+++ Express/templeteEngine.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("", (req, res) => {
+  res.render('Welcome');
+});
+
+app.listen(2000, () => {
+  console.log("Server is running on port 2000");
+});
